39e5295b55028ba376d8a5799dc10fa0dd13c66b
SAUMYA SINHA <saumyasinha269@gmail.com>
2024-06-06 05:19:17 +0530

    update

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -11,6 +11,27 @@ class QuestionsScreen extends StatefulWidget{
 class _QuestionsScreenState extends State<QuestionsScreen>{
   @override
   Widget build(context) {
-    return Text('QuestionScreen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+        const Text ('These are questions ...'),
+        const SizedBox(height: 30),
+        ElevatedButton(onPressed: () {},
+          style: ElevatedButton.styleFrom(),
+            child:const Text('Answer 1'),
+        ),
+        ElevatedButton(onPressed: () {},
+          child:const Text('Answer 2'),
+        ),
+        ElevatedButton(onPressed: () {},
+          child:const Text('Answer 3'),
+        ),
+        ElevatedButton(onPressed: () {},
+          child:const Text('Answer 4'),
+        ),
+      ]),
+    );
   }
 }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -20,6 +20,12 @@ class _QuizState extends State<Quiz>{
   }
   @override
   Widget build(context){
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if(activeScreen == 'questions-screen'){
+      screenWidget = const QuestionsScreen();
+    }
+    
     return  MaterialApp(
       home: Scaffold(
         body: Container(
@@ -33,9 +39,7 @@ class _QuizState extends State<Quiz>{
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-            ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );

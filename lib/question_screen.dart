9478d7991ead01273e54a6e1462a2d9ce915348e
SAUMYA SINHA <saumyasinha269@gmail.com>
2024-06-08 01:03:31 +0530

    update

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,37 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:quizz_mania/start_screen.dart';
-class QuestionsScreen extends StatefulWidget{
+import 'package:quizz_mania/answer_button.dart';
+import 'package:quizz_mania/data/questions.dart';
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
+
   @override
   State<QuestionsScreen> createState() {
-  return _QuestionsScreenState();
+    return _QuestionsScreenState();
   }
 }
 
-class _QuestionsScreenState extends State<QuestionsScreen>{
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex =0;
+
+  void answerQuestion() {
+
+    setState(() {
+      currentQuestionIndex++;
+    });
+
+  }
+
   @override
   Widget build(context) {
+    final currentQuestions = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        const Text ('These are questions ...'),
-        const SizedBox(height: 30),
-        ElevatedButton(onPressed: () {},
-          style: ElevatedButton.styleFrom(),
-            child:const Text('Answer 1'),
-        ),
-        ElevatedButton(onPressed: () {},
-          child:const Text('Answer 2'),
-        ),
-        ElevatedButton(onPressed: () {},
-          child:const Text('Answer 3'),
-        ),
-        ElevatedButton(onPressed: () {},
-          child:const Text('Answer 4'),
-        ),
-      ]),
+      child: Container(
+        margin:const  EdgeInsets.all(40),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            Text(
+              currentQuestions.text,
+              textAlign : TextAlign.center,
+            ),
+
+
+              const SizedBox(height: 30),
+              ...currentQuestions.getShuffledAnswers().map((answer) {
+                return AnswerButton(answerText: answer, onTap: answerQuestion);
+              }),
+
+            ],
+            ),
+      ),
     );
   }
 }
